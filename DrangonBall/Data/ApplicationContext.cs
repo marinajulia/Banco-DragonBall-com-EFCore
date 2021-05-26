2d3839498eae4c838ab24b4c599357b7f446e1a8
Marina Julia <marinajulia707@gmail.com>
2021-05-25 21:25:57 -0300

    Aplicação de contexto

---
+++ DrangonBall/Data/ApplicationContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrangonBall.Data {
+    public class ApplicationContext : DbContext {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            optionsBuilder.UseSqlServer((@"Data Source=DESKTOP-8024PRG\SERVIDOR;Initial Catalog=DragonBallEFCore;Integrated Security=True");
+        }
+    }
+}
