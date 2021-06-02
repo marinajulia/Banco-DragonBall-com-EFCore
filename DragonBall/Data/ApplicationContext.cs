c966b6e1c13573dd13e1092cc7db4303736267b7
Marina Julia <marinajulia707@gmail.com>
2021-06-02 13:12:20 -0300

    Criando banco de dados

--- DragonBall/Data/ApplicationContext.cs
+++ DragonBall/Data/ApplicationContext.cs
@@ -1,25 +1,21 @@
-using DragonBall.Models;
 using Microsoft.EntityFrameworkCore;
-
+using DragonBall.Models;
 
 namespace DragonBall.Data
 {
-    public class ApplicationContext : DbContext
+    public class DataContext : DbContext
     {
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
         public DbSet<Raca> Raca { get; set; }
         public DbSet<InfoRaca> InfoRaca { get; set; }
         public DbSet<Classe> Classe { get; set; }
         public DbSet<Personagem> Personagem { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-8024PRG\SERVIDOR;Initial Catalog=DragonBall;Integrated Security=True")
-        }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationContext).Assembly);
-        }
 
     }
 }
---
+++ DragonBall/Migrations/20210602161034_DragonBall.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DragonBall.Migrations
+{
+    public partial class DragonBall : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Classe",
+                columns: table => new
+                {
+                    ClasseId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DescricaoClasse = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Classe", x => x.ClasseId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Raca",
+                columns: table => new
+                {
+                    RacaId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Raca", x => x.RacaId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "InfoRaca",
+                columns: table => new
+                {
+                    InfoRacaId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DescricaoInfoRaca = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: false),
+                    RacaId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InfoRaca", x => x.InfoRacaId);
+                    table.ForeignKey(
+                        name: "FK_InfoRaca_Raca_RacaId",
+                        column: x => x.RacaId,
+                        principalTable: "Raca",
+                        principalColumn: "RacaId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Personagem",
+                columns: table => new
+                {
+                    PersonagemId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    PowerLevel = table.Column<int>(type: "int", maxLength: 10000, nullable: false),
+                    RacaId = table.Column<int>(type: "int", nullable: false),
+                    ClasseId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Personagem", x => x.PersonagemId);
+                    table.ForeignKey(
+                        name: "FK_Personagem_Classe_ClasseId",
+                        column: x => x.ClasseId,
+                        principalTable: "Classe",
+                        principalColumn: "ClasseId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Personagem_Raca_RacaId",
+                        column: x => x.RacaId,
+                        principalTable: "Raca",
+                        principalColumn: "RacaId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InfoRaca_RacaId",
+                table: "InfoRaca",
+                column: "RacaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Personagem_ClasseId",
+                table: "Personagem",
+                column: "ClasseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Personagem_RacaId",
+                table: "Personagem",
+                column: "RacaId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "InfoRaca");
+
+            migrationBuilder.DropTable(
+                name: "Personagem");
+
+            migrationBuilder.DropTable(
+                name: "Classe");
+
+            migrationBuilder.DropTable(
+                name: "Raca");
+        }
+    }
+}
